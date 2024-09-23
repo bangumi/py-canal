0c1c94750435f20be08fe3f73e708b7256d7215b
Trim21 <trim21.me@gmail.com>
2024-09-24 02:55:56 +0800

    remove redis broker

--- app/wiki_date/__init__.py
+++ app/wiki_date/__init__.py
@@ -1,9 +1,7 @@
 import contextlib
-from typing import Any, cast
 from dataclasses import dataclass
 from collections.abc import Iterable
 
-import redis
 import msgspec
 from kafka import KafkaConsumer
 from loguru import logger
@@ -13,7 +11,6 @@ from kafka.consumer.fetcher import ConsumerRecord
 from app import config
 from app.db import create_engine
 from app.model import Op, KafkaValue, SubjectType
-from app.config import redis_dsn
 from app.wiki_date.extract_date import extract_date
 
 
@@ -29,54 +26,7 @@ class ChiiSubject:
 decoder = msgspec.json.Decoder(KafkaValue[ChiiSubject])
 
 
-def __wiki_date_redis_events(dsn: str) -> Iterable[ChiiSubject]:
-    stream = "debezium.chii.bangumi.chii_subjects"
-    group_name = "canal-wiki-date4"
-    r = redis.Redis.from_url(dsn)
-    r.ping()
-
-    groups = {x["name"]: x for x in cast(list[dict[str, Any]], r.xinfo_groups(stream))}
-    if group_name.encode() not in groups:
-        r.xgroup_create(stream, group_name, "0", True)
-
-    last_msg_id = {
-        stream: groups.get(group_name.encode(), {}).get("last-delivered-id", ">")
-    }
-
-    while True:
-        changes = cast(
-            "list[Any]",
-            r.xreadgroup(
-                group_name, "py-canal", {stream: last_msg_id[stream]}, noack=True
-            ),
-        )
-        for change in changes:
-            stream_name, real_changes = change
-            for id, msg in real_changes:
-                value = msg[b"value"]
-                subject: KafkaValue[ChiiSubject] = decoder.decode(value)
-                before = subject.payload.before
-                after = subject.payload.after
-
-                if subject.payload.op == Op.Delete:
-                    continue
-
-                if after is None:
-                    continue
-
-                if before is None:
-                    yield after
-                    continue
-
-                if after.field_infobox != before.field_infobox:
-                    yield after
-
-
 def __wiki_date_kafka_events() -> Iterable[ChiiSubject]:
-    if redis_dsn:
-        yield from __wiki_date_redis_events(redis_dsn)
-        return
-
     consumer = KafkaConsumer(
         "debezium.chii.bangumi.chii_subjects",
         group_id="py-cache-clean",
@@ -113,7 +63,7 @@ def wiki_date() -> None:
 
     while True:
         for subject in __wiki_date_kafka_events():
-            logger.debug("event: subject wiki change {}", subject.subject_id)
+            logger.info("event: subject wiki change {}", subject.subject_id)
             try:
                 w = parse(subject.field_infobox)
             except WikiSyntaxError:
