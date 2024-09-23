69878402aa530ce639967718fc00a95a21ab6403
Trim21 <trim21.me@gmail.com>
2024-09-24 03:33:33 +0800

    add startup logger

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from threading import Thread
 
+from sslog import logger
+
+from app import config
 from app.wiki_date import wiki_date
 from app.clean_cache import clean_cache
 
 
 def main() -> None:
+    logger.info("app start", broker=config.broker)
     threads = [
         Thread(target=clean_cache),
         Thread(target=wiki_date),
